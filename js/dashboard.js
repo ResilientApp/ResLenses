0432f9de328707cf51f367e9acae9e72f87b28d8
Gia-Phong Nguyen <43556320+gjnguyen18@users.noreply.github.com>
2024-05-29 01:00:33 -0700

    .

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,11 +1,11 @@
-import * as T from 'three';
+// import * as T from 'three';
 import { getColorFromRamp } from './helpers.js';
 import { sceneInit } from './sceneInit.js';
 import { TransactionsGrid } from './transactionBlock.js';
 import { SceneControl } from './control.js';
 import { initUI } from './UIInit.js';
 import { getData } from './endpoint.js';
-import { Button } from './pageElements.js';
+// import { Button } from './pageElements.js';
 import { test } from './test.js';
 
 export function startScene() {
@@ -24,8 +24,8 @@ export function startScene() {
 
 	// default file
 	// let file = "https://raw.githubusercontent.com/gjnguyen18/utxo-lenses/master/transformed_data2.json"
-	// let file = "http://localhost:8080/getData_RESDB"
-	// let file = "http://localhost:8080/getData_ETH"
+	// let resDBLink = "http://localhost:3080/getData_RESDB"
+	// let ethLink = "http://localhost:3080/getData_ETH"
 
 	let resDBLink = "https://res-lenses-backend.resilientdb.com/getData_RESDB"
 	let ethLink = "https://res-lenses-backend.resilientdb.com/getData_ETH"
@@ -33,21 +33,21 @@ export function startScene() {
 	// console.log(encodeURIComponent("http://127.0.0.1:5501/api/v1/?startTime=2022-11-01T00:00:00Z&endTime=2023-11-30T23:59:59Z"))
 
 	// if query contains link, use that instead
-	let urlSearchParams = new URLSearchParams(window.location.search);
-	let params = Object.fromEntries(urlSearchParams.entries());
-	let link = params.link;
-	if (link) {
-		file = link;
-	} 
+	// let urlSearchParams = new URLSearchParams(window.location.search);
+	// let params = Object.fromEntries(urlSearchParams.entries());
+	// let link = params.link;
+	// if (link) {
+	// 	resDBLink = link;
+	// } 
 
-	getData(file, (data) => {
+	getData(resDBLink, (data) => {
 		transactionsGrid.loadData(data);
 		control.isDataLoaded = true;
 
 		fetch("../tooltips.json").then(response => response.text()).then(tooltipData => {
 			let tooltips = JSON.parse(tooltipData)
 			// console.log(tooltips)
-			initUI(transactionsGrid, control, data, tooltips);
+			initUI(transactionsGrid, control, data, tooltips, resDBLink, ethLink);
 		})
 	})
 
