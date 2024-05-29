364e4fe1075c9dfb32dcbcf0ed2a0a60df8e83fa
Gia-Phong Nguyen <43556320+gjnguyen18@users.noreply.github.com>
2024-05-29 02:58:06 -0700

    finally done

--- js/dashboard.js
+++ js/dashboard.js
@@ -8,6 +8,32 @@ import { getData } from './endpoint.js';
 // import { Button } from './pageElements.js';
 import { test } from './test.js';
 
+const tooltips = {
+    placeholder: "Placeholder text",
+    resetPosition: "Return to Origin: Reset the position of the camera to the top left corner",
+    dataSelect: "Data: Select the source of the data you wish to view.",
+    sortSelect: "Sort: Select how the addresses is sorted from top/left to bottom/right.",
+    viewSelect: "View: Select which format you wish to view the data in.",
+    scaleSelect: "Scale: Select the relative scaling of the data.",
+    symmetrySelect: "Symmetry: Select whether or not the transactions are symmetrical",
+
+    resDBData: "ResDB: This is live data from the Resilient DB network. This data is updated once every hour.",
+    ethData: "Ethereum: This is data procurred from the Ethereum Mainnet network. This data is the cummulation of random sampling of 1000 transactions every 30 minutes for the last 24 hours. Takes a few seconds to load.",
+
+    TransactionTotal: "Transactions Total: Sorts addresses based on the total summation of all UTXO they've sent out",
+    numTransactions: "Number of Transactions: Sorts addresses based on the number of transactions they've sent out",
+    largestTransaction: "Largest Transaction: Sorts the activity between all pairs of users first based on UTXO totals, then sorts addresses based high activity pairs. Recommended for highly sparse data such as Ethereum. Bars will likely form in a diagonal",
+
+    grid: "Grid: Display a 2D grid representing pairwise activity between address A and address B",
+    bar: "Bar: Display a bar graph representing the total activity of each address",
+
+    linear: "Linear: Bars are linearly scaled.",
+    log: "Log: Bars are logarithmically scaled.",
+
+    symmetric: "True: Transactions sent to and from an address are both accounted for and summed up regarding activity per From/To pair of addresses.",
+    asymmetric: "False: Only transactions sent to the recieving address is accounted for activity per From/To pair of addresses."
+}
+
 export function startScene() {
 
 	// scene init
@@ -44,11 +70,13 @@ export function startScene() {
 		transactionsGrid.loadData(data);
 		control.isDataLoaded = true;
 
-		fetch("../tooltips.json").then(response => response.text()).then(tooltipData => {
-			let tooltips = JSON.parse(tooltipData)
-			// console.log(tooltips)
-			initUI(transactionsGrid, control, data, tooltips, resDBLink, ethLink);
-		})
+		// fetch("../tooltips.json").then(response => response.text()).then(tooltipData => {
+		// 	let tooltips = JSON.parse(tooltipData)
+		// 	// console.log(tooltips)
+		// 	initUI(transactionsGrid, control, data, tooltips, resDBLink, ethLink);
+		// })
+
+		initUI(transactionsGrid, control, data, tooltips, resDBLink, ethLink);
 	})
 
 	// mouse events
